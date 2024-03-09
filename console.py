2e0c0b12766a8db22802d9fe63084890fae4fecf
deezyfg <mensahpeter421@gmail.com>
2024-03-09 08:49:03 -0800

    Added/Updated all tasks.

--- console.py
+++ console.py
@@ -11,7 +11,23 @@ import re
 from typing import cast
 
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Place": Place,
+    "Amenity": Amenity,
+    "Review": Review
+}
 
 class HBNBCommand(cmd.Cmd):
     """AirBnB clone console"""
